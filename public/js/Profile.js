77586f64780bc27eb524014f17f4f54b6cb8622d
Namnopro123 <phuongnamkante@gmail.com>
2023-10-06 15:42:36 +0700

    nop ho so project

--- public/js/Profile.js
+++ public/js/Profile.js
@@ -1,18 +1,4 @@
 
-// // Lấy tham chiếu đến các phần tử HTML cần thao tác
-// const nophosoLink = document.getElementById("nophoso-link");
-// const formNophoso = document.getElementById("Nophoso");
 
-// // Thêm sự kiện click cho liên kết "Nộp Hồ Sơ"
-// nophosoLink.addEventListener("click", function (event) {
-//     event.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
-
-//     // Kiểm tra nếu formNophoso đã ẩn thì hiển thị, ngược lại ẩn đi
-//     if (formNophoso.style.display === "none" || formNophoso.style.display === "") {
-//         formNophoso.style.display = "block"; // Hiển thị form
-//     } else {
-//         formNophoso.style.display = "none"; // Ẩn form
-//     }
-// });
 
 
